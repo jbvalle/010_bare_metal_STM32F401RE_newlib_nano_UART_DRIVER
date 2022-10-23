b91b37c2ed9c9a0a9eefa229943ba6b03b701130
John Bryan Valle <jbvalle.jv@gmail.com>
2022-10-23 05:14:31 +0200

    added steps for uart driver

--- src/main.c
+++ src/main.c
@@ -7,7 +7,24 @@ USART_t * const USART2 = (USART_t *) 0x40004400;
 
 int main(void){
 
-    
+    /** Enable CLOCK for GPIOA **/
+    /** Enable CLOCK for USART2 **/
 
-    for(;;);
+    /** SET AF07 for PA2 and PA3 **/
+    /** SET AF MODE for PA2 and PA3 **/
+
+    /** Set Baud Rate Register **/
+    /** SET CR1 -> CR1[3, 2] Enable TXE RXE **/
+
+    /** SET CR2 -> CR[13:12] Set 00 for 1 STOP BIT **/
+
+    /** RESET CR3 -> Disable flow control RTS CTS **/
+
+    /** Enable UART CR1 **/
+
+    for(;;){
+        /** Wait until TXE Empty SR **/
+
+        /** Write to DR **/
+    }
 }
